5ee759dae913df6bd1d1769226ced3ab956ab755
adrienctl <adrien.coutelle@telecom-paris.fr>
2026-04-05 14:15:57 +0200

    device

---
+++ gaussian_splatting/structures/device.py
@@ -0,0 +1,27 @@
+import torch
+
+from gaussian_splatting.utils.logger import Logger
+
+logger = Logger("DEVICE")
+
+
+class Device:
+    device = None
+
+    @classmethod
+    def get(cls) -> torch.device:
+        if cls.device is not None:
+            return cls.device
+
+        if torch.backends.mps.is_available():
+            cls.device = torch.device("mps")
+            torch.set_default_dtype(torch.float32)
+            logger.info("Using device: MPS (Apple Silicon)")
+        elif torch.cuda.is_available():
+            cls.device = torch.device("cuda")
+            logger.info("Using device: CUDA")
+        else:
+            cls.device = torch.device("cpu")
+            logger.info("Using device: CPU")
+
+        return cls.device
