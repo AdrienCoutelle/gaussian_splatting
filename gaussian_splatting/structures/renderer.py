66c7cb03f3640883c30cd7aa9ddb489b36a14f67
adrienctl <adrien.coutelle@telecom-paris.fr>
2026-04-05 21:01:14 +0200

    camera

--- gaussian_splatting/structures/renderer.py
+++ gaussian_splatting/structures/renderer.py
@@ -3,27 +3,8 @@ from dataclasses import dataclass
 import numpy as np
 import torch
 
+from gaussian_splatting.structures.camera import Camera
 from gaussian_splatting.structures.gaussian import Gaussian
-
-
-@dataclass
-class Camera:
-    pose: torch.Tensor
-    focal_length: float
-    width: int
-    height: int
-
-    @property
-    def f(self) -> float:
-        return self.focal_length
-
-    @property
-    def h(self) -> int:
-        return self.height
-
-    @property
-    def w(self) -> int:
-        return self.width
 
 
 @dataclass
